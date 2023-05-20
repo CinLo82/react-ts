c3d28d5a016e4bf1b7f04134ca52ba7a79e32ea9
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-20 20:30:33 -0300

    configurando el proyecto

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline ">
+      Hello Cinlo!
+      </h1>
+    </main>
+  )
+}
