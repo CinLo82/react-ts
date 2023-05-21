34fa7d5b45ced9f53508971a0a5ba7de688ce00f
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-20 22:50:38 -0300

    el objeto props y children

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
+
+import { RandomFox } from '@components/RandomFox'
+
+// Generates a random number between 1 and 122
+const randomNumber = () => Math.floor(Math.random() * 122) + 1;
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline ">
       Hello Cinlo!
+      <RandomFox image={`https://randomfox.ca/images/${randomNumber()}.jpg`}/>
       </h1>
     </main>
   )
