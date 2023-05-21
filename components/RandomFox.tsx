36e335327cb76a35448c8c3eb7f914f5db164838
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-21 11:26:20 -0300

    Lazy loading con observadores

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,17 +1,43 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = { image:string };
 
 
 export const RandomFox = ({ image }: Props): JSX.Element =>{
   const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+  "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+  useEffect(() => {
+    //nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+      //onIntersection => console.log
+          console.log('hey you!');
+        }
+      })
+    })
+     // observe nodo
+     if(node.current) {
+      observer.observe(node.current);
+     }
+    
+    // desconectar
+    return (() => {
+      observer.disconnect();
+    })
+  }, [image])
+ 
+  
 
   return (
     <img
       ref={node}
       width="320"
       height="auto"
-      src={image}
+      src={src}
       className="mx-auto rounded-md bg-gray-300"
     />
   );
